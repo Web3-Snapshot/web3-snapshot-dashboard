58cdd3fe03b16d027b16dd58c83978085986f78a
Magnus Henkel <loxosceles@gmx.de>
2022-11-20 17:04:27 +0000

    add a rudimentary fetch button

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Routes, Route } from 'react-router-dom'
-import Coins from './components/Coins';
-import Coin from './routes/Coin';
-import Navbar from './components/Navbar';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import Coins from "./components/Coins";
+import Coin from "./routes/Coin";
+import Navbar from "./components/Navbar";
+
+const URL1 =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+// TODO: this should be the other URL from which we fetch the alternative data
+const URL2 = "https://fake-url.com";
+
+async function fetchData(url) {
+  return axios
+    .get(url)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+}
 
 const App = () => {
-
-  const [coins, setCoins] = useState([])
-
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  const [coins, setCoins] = useState([]);
+  const [fetchUrl, setFetchUrl] = useState(URL1);
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setCoins(response.data)
-     //console.log(response.data[0])
-    }).catch((error) => {
-      console.log(error)
-    })
-  }, [])
+    async function fetch() {
+      fetchData(fetchUrl).then((res) => {
+        setCoins(res);
+      });
+    }
 
+    fetch();
+  }, [fetchUrl]);
+
+  function handleTableUpdate(evt) {
+    if (fetchUrl === URL1) {
+      setFetchUrl(URL1);
+      console.log(`fetching ${URL1}`);
+    } else {
+      // TODO: only when you change the 'fetchUrl` here, the 'useEffect`
+      // will run and the new data will be fetched
+      // setFetchUrl(URL2);
+      console.log(`fetching ${URL2}`);
+    }
+  }
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Coins coins={coins} />} />
+        <Route
+          path='/'
+          element={<Coins clickHandler={handleTableUpdate} coins={coins} />}
+        />
         <Route path='/coin' element={<Coin />}>
           <Route path=':coinId' element={<Coin />} />
         </Route>
       </Routes>
-     
     </>
   );
-}
-
+};
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import {FaCoins} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-	return (
-		<Link to='/'>
-			<div className='navbar'>
-				<FaCoins className='icon' />
-				<h1> Marco <span className='purple'>Web3</span> Space</h1>
-			</div>
-		</Link>
-	)
-}
+  return (
+    <Link to='/'>
+      <div className='navbar'>
+        <FaCoins className='icon' />
+        <h1>
+          {" "}
+          Marco <span className='purple'>Web3</span> Space
+        </h1>
+      </div>
+    </Link>
+  );
+};
 
-export default Navbar
+export default Navbar;
