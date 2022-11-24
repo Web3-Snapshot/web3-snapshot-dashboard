c82dc14e92fa2447763eb3ecce733c73f0cabdf1
Magnus Henkel <loxosceles@gmx.de>
2022-11-24 23:15:20 +0000

    provide fixes for buttons and loop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
-import Coins from "./components/Coins-1";
+import Coins from "./components/Coins-2";
 import Coin from "./routes/Coin";
 import Navbar from "./components/Navbar";
+
+async function fetchData(url) {
+  return axios
+    .get(url)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+}
 
 const App = () => {
   const [coins, setCoins] = useState([]);
@@ -11,38 +18,42 @@ const App = () => {
   const url1 = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${totCryptos}&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2030d%2C%20200d%2C%201y%2C%203y`;
 
   useEffect(() => {
-    axios
-      .get(url1)
-      .then((response) => {
-        setCoins(response.data);
-        //console.log(response.data[0])
-      })
-      .catch((error) => {
-        console.log(error);
+    async function fetch() {
+      fetchData(url1).then((res) => {
+        setCoins(res);
       });
-  }, []);
+    }
 
-  // the following function should make a loop for "totCryptos" and return all data for each coin
+    fetch();
+  }, [url1]);
 
+  /**
+   * The following function loops through the first items of coins and returns
+   * some of the properties.
+   *
+   * const url2 = `https://api.coingecko.com/api/v3/coins/${params.coinId}`;
+   * this one needs a function to extract every params
+   */
   function coinData() {
-    // a loop through 100 coins to get their internal data
-    // const url2 = `https://api.coingecko.com/api/v3/coins/${params.coinId}`; this one needs a function to extract every params
-    const contCoins = totCryptos.map((item) => {
-      return <contCoins key={item.id} />;
+    return coins.slice(0, 10).map((coin) => {
+      return (
+        <>
+          <div>{coin.id}</div>
+          <div>{coin.symbol}</div>
+        </>
+      );
     });
-    return console.log(contCoins);
   }
-
-  // coinData();
 
   return (
     <>
       <Navbar />
-      <div className="grid"></div>
+      <div>{coinData()}</div>
+      <div className='grid'></div>
       <Routes>
-        <Route path="/" element={<Coins coins={coins} />} />
-        <Route path="/coin" element={<Coin />}>
-          <Route path=":coinId" element={<Coin />} />
+        <Route path='/' element={<Coins coins={coins} />} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
         </Route>
       </Routes>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,48 @@
-import React from 'react';
-import {FaCoins} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-import './Navbar.css';
-
+import React, { useState } from "react";
+import { FaCoins } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-	return (
-		<div>
-			<Link to='/'>
-				<div className='navbar'>
-					<FaCoins className='icon' />
-					<h1> Web3 <span className='purple'>Smart</span> Dashboard</h1>
-				</div>
-			</Link>
-			<div className='mainMenu'>
-				<button onClick={changePage1} id="btn1" className="btnPressed">PRICES</button>
-				<button onClick={changePage2} id="btn2" >SUPPLY</button>
-			</div>
-		</div>
-	);
+  const [toggleState, setToggleState] = useState("state1");
+
+  function handlePricesButton(evt) {
+    setToggleState("state2");
+    // fetch prices data here
+  }
+
+  function handleSupplyButton(evt) {
+    setToggleState("state1");
+    // fetch supply data here
+  }
+
+  return (
+    <div>
+      <Link to='/'>
+        <div className='navbar'>
+          <FaCoins className='icon' />
+          <h1>
+            {" "}
+            Web3 <span className='purple'>Smart</span> Dashboard
+          </h1>
+        </div>
+      </Link>
+      <div className='mainMenu'>
+        <button
+          className={toggleState === "state1" ? "active" : ""}
+          onClick={handlePricesButton}
+        >
+          PRICES
+        </button>
+        <button
+          className={toggleState === "state2" ? "active" : ""}
+          onClick={handleSupplyButton}
+        >
+          SUPPLY
+        </button>
+      </div>
+    </div>
+  );
 }
 
-
-
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-
-function changePage1() {
-	// btn1.classList.remove("btnPressed");
-	// btn2.classList.add("btnPressed");
-	console.log(this)
-	console.log(btn2)
-  }
-
-  function changePage2() {
-    // btn2.classList.remove("btnPressed");
-	// btn1.classList.add("btnPressed");
-  }
-
-  export default Navbar
+export default Navbar;
