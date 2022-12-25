bebf1e0b9594031e2d091883b25367e6b43b36c6
Magnus Henkel <loxosceles@gmx.de>
2022-12-25 16:06:01 +0100

    Generate tables dynamically

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,42 +1,86 @@
-import React, { useState, useEffect } from "react";
-import CoinItem1 from "./CoinItem-1";
-import Coin from "../routes/Coin";
-import { Link, useOutletContext } from "react-router-dom";
+import React from "react";
+import { useOutletContext } from "react-router-dom";
 
-import "./Coins.css";
+import Row from "./Row";
+import styles from "./Prices.module.css";
+import IconAndCurrencyIdCell from "./IconAndCurrencyIdCell";
+
+// url3 is supposed to fetch data for all coins at the 1 Jan 2020 date, and display the difference in %
+// const URL3 = `https://api.coingecko.com/api/v3/coins/bitcoin/history?date=1-1-2020`;
+
+// async function fetchData(url) {
+//   return axios
+//     .get(url)
+//     .then((res) => res.data)
+//     .catch((err) => console.log(err));
+// }
 
 function Prices() {
-  const { coins, coinProperties } = useOutletContext();
+  const { coinProperties } = useOutletContext();
 
-  const headCells = [
-    { id: "number", label: "#" },
-    { id: "coin", label: "Coin" },
-    { id: "price", label: "Price" },
-    { id: "1d", label: "1 Days" },
-    { id: "1w", label: "7 Days" },
-    { id: "1m", label: "30 Days" },
-    { id: "6m", label: "60 Days" },
-    { id: "1y", label: "200 Days" },
-    { id: "refDate", label: "01/01/20" },
+  const tableData = [
+    {
+      id: "number",
+      label: "#",
+      render: (obj) => obj.market_data.market_cap_rank,
+    },
+    {
+      id: "coin",
+      label: "Coin",
+      render: (obj) => <IconAndCurrencyIdCell obj={obj} />,
+    },
+    {
+      id: "1d",
+      label: "1 Days",
+      render: (obj) => obj.market_data.current_price.usd.toLocaleString(),
+    },
+    {
+      id: "1w",
+      label: "7 Days",
+      render: (obj) =>
+        obj.market_data.price_change_percentage_24h_in_currency.usd.toFixed(2),
+    },
+    {
+      id: "1m",
+      label: "30 Days",
+      render: (obj) =>
+        obj.market_data.price_change_percentage_7d_in_currency.usd.toFixed(2),
+    },
+    {
+      id: "6m",
+      label: "60 Days",
+      render: (obj) =>
+        obj.market_data.price_change_percentage_30d_in_currency.usd.toFixed(2),
+    },
+    {
+      id: "1y",
+      label: "200 Days",
+      render: (obj) =>
+        obj.market_data.price_change_percentage_60d_in_currency.usd.toFixed(2),
+    },
+    {
+      id: "refDate",
+      label: "01/01/20",
+      render: (obj) =>
+        obj.market_data.price_change_percentage_200d_in_currency.usd,
+    },
   ];
 
   return (
-    <div className='container'>
-      {coins && (
-        <div className='heading'>
-          {headCells.map((cell) => (
-            <p key={cell.id}>{cell.label}</p>
-          ))}
-        </div>
-      )}
-
-      <div className='table'>
-        {coins.map((coin) => (
-          <Link to={`/coin/${coin.id}`} element={<Coin />} key={coin.id}>
-            <CoinItem1 additionalInfo={coinProperties[coin.id]} />
-          </Link>
+    <div className={styles.container}>
+      <div className={`${styles.row} ${styles.header}`}>
+        {tableData.map((item) => (
+          <span className={styles.cell} key={item.id}>
+            {item.label}
+          </span>
         ))}
       </div>
+
+      {Object.entries(coinProperties).map(([id, coin]) => (
+        <div className={`${styles.row} ${styles.data}`}>
+          <Row key={id} tableData={tableData} row={coin} />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Supply.jsx
+++ src/components/Supply.jsx
@@ -1,83 +1,65 @@
 import React from "react";
 import Row from "./Row";
 import { useOutletContext } from "react-router-dom";
+import IconAndCurrencyIdCell from "./IconAndCurrencyIdCell";
 
-import "./Coins.css";
-
-function IconAndCurrencyId({ additionalInfo }) {
-  return (
-    <div className='img-symbol coin-col-2'>
-      <img src={additionalInfo.image.thumb} alt='coin' />
-      <p>{additionalInfo.symbol.toUpperCase()}</p>
-    </div>
-  );
-}
+import styles from "./Supply.module.css";
 
 function Supply() {
-  const { coins, coinProperties } = useOutletContext();
+  const { coinProperties } = useOutletContext();
 
-  const cellHeaders = [
-    "#",
-    "Coin",
-    "Market Cap",
-    "FD Market Cap",
-    "Total Supply",
-    "Max Supply",
-    "Total Volume",
+  const tableData = [
+    {
+      id: "marketCapRank",
+      label: "#",
+      render: (obj) => obj.market_data.market_cap_rank,
+    },
+    {
+      id: "icon",
+      label: "Coin",
+      render: (obj) => <IconAndCurrencyIdCell obj={obj} />,
+    },
+    {
+      id: "marketCapUSD",
+      label: "Market Cap",
+      render: (obj) => `$${obj.market_data.market_cap.usd.toLocaleString()}`,
+    },
+    {
+      id: "FDMarketCap",
+      label: "FD Market Cap",
+      render: (obj) => `$${obj.market_data.market_cap.usd.toLocaleString()}`,
+    },
+    {
+      id: "totalSupply",
+      label: "Total Supply",
+      render: (obj) => obj.market_data.total_supply,
+    },
+    {
+      id: "maxSupply",
+      label: "Max Supply",
+      render: (obj) => obj.market_data.max_supply,
+    },
+    {
+      id: "totalVolumeUSD",
+      label: "Total Volume",
+      render: (obj) => obj.market_data.total_volume.usd.toLocaleString(),
+    },
   ];
 
-  function getCellData(additionalInfo) {
-    return [
-      {
-        id: "marketCapRank",
-        value: additionalInfo.market_data.market_cap_rank,
-      },
-      {
-        id: "icon",
-        value: <IconAndCurrencyId additionalInfo={additionalInfo} />,
-      },
-      {
-        id: "marketCapUSD",
-        value: `$${additionalInfo.market_data.market_cap.usd.toLocaleString()}`,
-      },
-      {
-        id: "FDMarketCap",
-        value: `$${additionalInfo.market_data.market_cap.usd.toLocaleString()}`,
-      },
-      {
-        id: "totalSupply",
-        value: additionalInfo.market_data.total_supply,
-      },
-      {
-        id: "maxSupply",
-        value: additionalInfo.market_data.max_supply,
-      },
-      {
-        id: "totalVolumeUSD",
-        value: additionalInfo.market_data.total_volume.usd.toLocaleString(),
-      },
-    ];
-  }
-
   return (
-    <div className='container'>
-      {coins && (
-        <div>
-          <div className='row header'>
-            {cellHeaders.map((header) => (
-              <span className='cell'>{header}</span>
-            ))}
-          </div>
-
-          {coins.map((coin, idx) => {
-            return (
-              <div className='row data'>
-                <Row key={coin.id} row={getCellData(coinProperties[coin.id])} />
-              </div>
-            );
-          })}
+    <div className={styles.container}>
+      <div className={`${styles.row} ${styles.header}`}>
+        {tableData.map((item) => (
+          <span key={item.id} className={styles.cell}>
+            {item.label}
+          </span>
+        ))}
+      </div>
+      {Object.entries(coinProperties).map(([id, coin]) => (
+        <div className={`${styles.row} ${styles.data}`}>
+          <Row key={id} tableData={tableData} row={coin} />
         </div>
-      )}
+      ))}
     </div>
   );
 }
