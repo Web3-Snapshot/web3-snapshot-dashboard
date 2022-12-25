dc9cab66e7478f4609b636f5827ef4bd89a63fb0
Magnus Henkel <loxosceles@gmx.de>
2022-12-25 16:04:27 +0100

    Clean up App.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Prices from "./components/Prices";
 import Supply from "./components/Supply";
-import Coin from "./routes/Coin";
 import Navbar from "./components/Navbar";
-import { PAGES } from "./constants";
 import Dashboard from "./pages/Dashboard";
 import { fetchCoins } from "./pages/Dashboard";
 
@@ -13,45 +11,10 @@ export async function loader() {
   return { coins };
 }
 
-// url3 is supposed to fetch data for all coins at the 1 Jan 2020 date, and display the difference in %
-// const URL3 = `https://api.coingecko.com/api/v3/coins/bitcoin/history?date=1-1-2020`;
-
-// async function fetchData(url) {
-//   return axios
-//     .get(url)
-//     .then((res) => res.data)
-//     .catch((err) => console.log(err));
-// }
-
 function App() {
-  /**
-   * The following function loops through the first items of coins and returns
-   * some of the properties.
-   *
-   *
-   * we need to loop thru url2, not url1
-   */
-  function coinData(markets) {
-    return markets.slice(0, PAGES).map((coin) => {
-      return (
-        <>
-          <div>
-            {coin.symbol}: 1day: {coin.price_change_percentage_24h.toFixed(2)}%
-            {/* 1week: {coin.price_change_percentage_7d}%  1month: */}
-            {coin.price_change_percentage_30d} {/* 7months:{" "} */}
-            {coin.price_change_percentage_200d}
-            {/* 1year: */}
-            {coin.price_change_percentage_1y}
-          </div>
-        </>
-      );
-    });
-  }
-
   return (
     <>
       <Navbar />
-      <div className='grid'></div>
       <Routes>
         <Route path='/' element={<Dashboard />}>
           <Route index element={<Navigate to='prices' />} />
