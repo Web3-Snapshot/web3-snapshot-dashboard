61249691515995358b983edbf79e24ec99a2ad3d
Magnus Henkel <loxosceles@gmx.de>
2023-01-20 11:22:21 +0000

    Write HeaderRow component

---
+++ frontend/src/components/HeaderRow.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+
+const Row = ({ headers, row, styles, tableStyles, sortHandler, order, orderBy }) => {
+  return headers.map((cell) => (
+    <div key={cell.id} className={styles.cell}>
+      <FaAngleDown
+        className={`${tableStyles.iconDown} ${
+          order === 'desc' && orderBy === cell.id && tableStyles.iconActive
+        }`}
+        onClick={(evt) => sortHandler(evt, cell.id, 'desc')}
+      />
+      <span className={tableStyles.headerText}>{cell.label}</span>
+      <FaAngleUp
+        className={`${tableStyles.iconUp} ${
+          order === 'asc' && orderBy === cell.id && tableStyles.iconActive
+        }`}
+        onClick={(evt) => sortHandler(evt, cell.id, 'asc')}
+      />
+    </div>
+  ));
+};
+
+Row.defaultProps = {
+  row: [],
+  header: false,
+};
+
+export default Row;
