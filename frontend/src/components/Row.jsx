56936e03c2dde2c8224cdf14f4cc3804a084bcb4
Magnus Henkel <loxosceles@gmx.de>
2023-01-29 18:29:25 +0000

    Create cell overlay

--- frontend/src/components/Row.jsx
+++ frontend/src/components/Row.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
 const Row = ({ tableData, row, styles }) => {
-  return tableData.map((cell) => (
-    <div key={cell.id} className={styles.cell}>
-      {/* <div
-        className={styles.stacked}
-        style={{
-          position: 'absolute',
-          opacity: 0.3,
-          backgroundColor: 'red',
-          height: '4rem',
-          // width: '100%',
-        }}>
-        Stacked
-      </div> */}
-      {cell.render(row)}
-    </div>
-  ));
+  return (
+    <>
+      {tableData.slice(0, 2).map((cell) => (
+        <div key={cell.id} className={styles.cell}>
+          {cell.render(row)}
+        </div>
+      ))}
+      {tableData.slice(2).map((cell) => (
+        <div key={cell.id} className={styles.cell}>
+          <div
+            style={{
+              position: 'absolute',
+              left: 0,
+              backgroundColor: 'red',
+              opacity: 0.2,
+              width: '50%',
+              height: '100%',
+            }}></div>
+          <div>{cell.render(row)}</div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 Row.defaultProps = {
