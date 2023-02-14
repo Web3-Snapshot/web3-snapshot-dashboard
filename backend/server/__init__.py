4c036c581356f2d83cd93476e102ea73a2859320
Magnus Henkel <loxosceles@gmx.de>
2023-02-14 21:51:23 +0000

    Remove Marshmellow

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,9 +1,6 @@
 from os import environ
 
 from flask import Flask
-from flask_marshmallow import Marshmallow
-
-ma = Marshmallow()
 
 
 class PrefixMiddleware(object):
@@ -22,28 +19,13 @@ class PrefixMiddleware(object):
             return ["This url does not belong to the app.".encode()]
 
 
-def enable_debugger():
-    """Starts the debug server for Visual Studio Code on port 5678
-    and waits until the debugger attaches"""
-    import debugpy
-
-    # 5678 is the default attach port in the VS Code debug configurations
-    print("Waiting for debugger attach")
-    debugpy.listen(("localhost", 5678))
-    debugpy.wait_for_client()
-
-
 def create_app(config_location="config.development"):
-    if environ.get("VSCODE_DEBUGGER"):
-        enable_debugger()
 
     app = Flask(__name__, instance_relative_config=True)
     app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
 
     app.config.from_object(config_location)
     print(app.config)
-
-    ma.init_app(app)
 
     from .db import close_connection
 
