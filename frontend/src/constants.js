ceb8e1adce33d307c538ffc7b7b140fae3817fe8
Magnus Henkel <loxosceles@gmx.de>
2023-02-28 00:06:02 +0000

    Improve breakpoins and remove duplicate definitions

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,11 +1,18 @@
+import styles from './breakpoints.scss';
+
+const [ss, xs, sm, md, lg] = [+styles.ssBp, +styles.xsBp, +styles.smBp, +styles.mdBp, +styles.lgBp];
+
 export const COINS = 2;
 
-// Material UI default breakpoints
+// Material UI default breakpoints: (xs - 600, sm - 900, md - 1200, lg - 1536)
 export const BREAKPOINTS = {
-  ss: { min: 0, max: 400 },
-  xs: { min: 401, max: 600 },
-  sm: { min: 601, max: 900 },
-  md: { min: 901, max: 1200 },
-  lg: { min: 1201, max: 1535 },
-  xl: { min: 1536, max: null },
+  ss: { min: 0, max: ss }, // super small < 400
+  xs: { min: ss + 1, max: xs },
+  sm: { min: xs + 1, max: sm },
+  md: { min: sm + 1, max: md },
+  lg: { min: md + 1, max: lg },
+  xl: { min: lg + 1, max: null },
+  mobile: { min: 0, max: sm },
+  tablet: { min: sm + 1, max: md },
+  desktop: { min: md + 1, max: null },
 };
