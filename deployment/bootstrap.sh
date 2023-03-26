cdc578a5dc0da1d6acf5a58f91227f698f5406cd
Magnus Henkel <loxosceles@gmx.de>
2023-03-26 19:09:15 +0200

    small fixes in nginx config

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -21,8 +21,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
-  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt update
 
