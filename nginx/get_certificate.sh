4ec2867a5016b1679d8d93b0915228ad726937be
loxosceles <loxosceles@mailbox.org>
2023-07-16 12:33:30 +0000

    Create renewal Makefile commands

--- nginx/get_certificate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOMAIN=web3snapshot.loxosceles.me
-
-docker compose run --rm  certbot certonly --webroot \
-    --webroot-path /var/www/certbot/ --dry-run \
-    -d www.${DOMAIN} -d ${DOMAIN} -v
