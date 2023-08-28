8be9dd3b0b39c268e8f61df39122231a6ca1faa3
Magnus Henkel <loxosceles@mailbox.org>
2023-08-28 20:41:45 +0000

    Remove redundant lines

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Prices from './components/Prices';
 import Tokenomics from './components/Tokenomics';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
-import { fetchCoins } from './pages/Dashboard';
 import { useIsIframe } from './custom-hooks/useIsIframe';
 
-export async function loader() {
-  const coins = await fetchCoins();
-  return { coins };
-}
-
 function App() {
-
-
-
   const isIframe = useIsIframe();
   return (
     <>
