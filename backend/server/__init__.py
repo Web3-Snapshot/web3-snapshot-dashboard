73de4192b218018b8850d15963ade3b60db24a9a
Magnus Henkel <loxosceles@mailbox.org>
2023-08-29 20:37:50 +0000

    Fix linter

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -9,7 +9,6 @@ class PrefixMiddleware(object):
         self.prefix = prefix
 
     def __call__(self, environ, start_response):
-
         if environ["PATH_INFO"].startswith(self.prefix):
             environ["PATH_INFO"] = environ["PATH_INFO"][len(self.prefix) :]
             environ["SCRIPT_NAME"] = self.prefix
@@ -30,7 +29,6 @@ def enable_debugger():
 
 
 def create_app(config_env="server.config.development"):
-
     print("Debug option set to", environ.get("DEBUG"))
     if environ.get("DEBUG") == "on":
         enable_debugger()
@@ -42,9 +40,11 @@ def create_app(config_env="server.config.development"):
     print(app.config)
 
     from .db import close_connection
+
     app.teardown_appcontext(close_connection)
 
     from server.routes import coins
+
     app.register_blueprint(coins.bp)
 
     return app
