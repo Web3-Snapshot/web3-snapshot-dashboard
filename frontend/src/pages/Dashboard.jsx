1f09b6f48f193dc1a28c3ad5e0243a53677bd9ad
Magnus Henkel <loxosceles@proton.org>
2023-12-17 21:05:30 +0000

    Remove static data fetch from Dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,54 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import { isEmpty } from 'lodash';
+import React, { useEffect } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import styles from './Dashboard.module.scss';
 import navbarStyles from '../components/Navbar.module.scss';
 import DisclaimerMessage from '../components/DisclaimerMessage';
 import { useIsIframe } from '../custom-hooks/useIsIframe';
-import { normalizeResponse } from '../util/helpers';
-
-async function fetchCoins() {
-  return axios
-    .get('/api/coins')
-    .then((res) => {
-      console.log(res.data);
-      return res.data;
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
-}
+import { usePricesStore } from '../components/Prices/state';
 
 function Dashboard() {
-  const [coins, setCoins] = useState({ data: {}, order: [] });
   const isIframe = useIsIframe();
-  const initalDataFetchDone = useRef(false);
+  const setRows = usePricesStore((state) => state.setRows);
+  const setUpdatedAt = usePricesStore((state) => state.setUpdatedAt);
 
   useEffect(() => {
-    const fetchData = async function () {
-      fetchCoins().then((res) => {
-        console.log(res);
-        const normalizedRes = normalizeResponse(res);
-        setCoins(normalizedRes);
-      });
-    };
-
-    fetchData();
-    initalDataFetchDone.current = true;
-  }, []);
-
-  useEffect(() => {
-    if (!initalDataFetchDone.current) return;
-
+    // if (!initalDataFetchDone.current) return;
     const sse = new EventSource('/api/coin-stream');
 
     function handleStream(evt) {
       console.log('evt.data', evt.data);
       const res = JSON.parse(evt.data);
-      const normalizedRes = normalizeResponse(res);
-      console.log('normalizedRes', normalizedRes);
-      setCoins(normalizedRes);
+      setRows(res.payload);
+      setUpdatedAt(res.updated_at);
     }
 
     sse.onmessage = (evt) => {
@@ -84,12 +55,10 @@ function Dashboard() {
           </NavLink>
         </nav>
       )}
-      {!isEmpty(coins.data) && (
-        <main>
-          <DisclaimerMessage />
-          <Outlet context={{ coins }} />
-        </main>
-      )}
+      <main>
+        <DisclaimerMessage />
+        <Outlet />
+      </main>
     </div>
   );
 }
