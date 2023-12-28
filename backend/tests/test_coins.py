7f36cf8fa293b477b1100e7bf62481257a446e8b
Magnus Henkel <loxosceles@proton.org>
2023-12-28 22:53:32 +0000

    Make a few changes and add docs to tests

--- backend/tests/test_coins.py
+++ backend/tests/test_coins.py
@@ -1,4 +1,5 @@
 import json
+from asyncio import sleep
 from contextlib import contextmanager
 from datetime import datetime, timezone
 from functools import partial
@@ -11,17 +12,10 @@ from database_utils.helpers import (
     normalize_coins,
     process_percentages,
 )
+from fakeredis import FakeStrictRedis
+from rq import Queue
+from rq.job import Job
 from server.routes.coins import event_stream
-
-
-@contextmanager
-def mock_events():
-    with mock.patch(
-        "server.routes.coins.event_stream",
-        partial(event_stream, single=True),
-    ):
-        yield
-
 
 UPDATED_AT = datetime(2022, 1, 1, 0, 0, 0, tzinfo=timezone.utc).isoformat()
 
@@ -62,6 +56,18 @@ INSERT_FIELDS = [
     "price_change_percentage_1y_in_currency",
     "updated_at",
 ]
+
+
+@contextmanager
+def mock_events():
+    with mock.patch(
+        "server.routes.coins.event_stream",
+        partial(
+            event_stream,
+            single=True,
+        ),
+    ):
+        yield
 
 
 def seed_coin(conn, app):
@@ -123,71 +129,102 @@ def seed_coin(conn, app):
 
 
 def test_get_coins(client, db_connection, app):
+    """Test case for the 'get_coins' endpoint.
+
+    This test verifies that the 'get_coins' endpoint returns the expected JSON response.
+
+    Args:
+        client (TestClient): The test client for making HTTP requests.
+        db_connection (DatabaseConnection): The database connection object.
+        app (Flask.app): The Flask application object.
+
+    Returns:
+        None
+    """
     seed_coin(db_connection, app)
 
-    with mock_events():
-        response = client.get("/api/coins")
+    response = client.get("/api/coins")
 
-        assert response.json == {
-            "prices": {
-                "test_id": {
-                    "ath_change_percentage": 1.23,
-                    "ath_change_percentage_relative": 100,
-                    "current_price": 0.123,
-                    "id": "test_id",
-                    "image": "test_img_url",
-                    "market_cap_rank": 1,
-                    "name": "test_name",
-                    "price_change_percentage_1h_in_currency": 1.23,
-                    "price_change_percentage_1h_in_currency_relative": 100,
-                    "price_change_percentage_1y_in_currency": 1.23,
-                    "price_change_percentage_1y_in_currency_relative": 100,
-                    "price_change_percentage_24h_in_currency": 1.23,
-                    "price_change_percentage_24h_in_currency_relative": 100,
-                    "price_change_percentage_30d_in_currency": 1.23,
-                    "price_change_percentage_30d_in_currency_relative": 100,
-                    "price_change_percentage_7d_in_currency": 1.23,
-                    "price_change_percentage_7d_in_currency_relative": 100,
-                    "symbol": "test_symbol",
-                }
+    assert response.json == {
+        "prices": {
+            "test_id": {
+                "ath_change_percentage": 1.23,
+                "ath_change_percentage_relative": 100,
+                "current_price": 0.123,
+                "id": "test_id",
+                "image": "test_img_url",
+                "market_cap_rank": 1,
+                "name": "test_name",
+                "price_change_percentage_1h_in_currency": 1.23,
+                "price_change_percentage_1h_in_currency_relative": 100,
+                "price_change_percentage_1y_in_currency": 1.23,
+                "price_change_percentage_1y_in_currency_relative": 100,
+                "price_change_percentage_24h_in_currency": 1.23,
+                "price_change_percentage_24h_in_currency_relative": 100,
+                "price_change_percentage_30d_in_currency": 1.23,
+                "price_change_percentage_30d_in_currency_relative": 100,
+                "price_change_percentage_7d_in_currency": 1.23,
+                "price_change_percentage_7d_in_currency_relative": 100,
+                "symbol": "test_symbol",
+            }
+        },
+        "tokenomics": {
+            "test_id": {
+                "circ_supply_total_supply_ratio": 1.0,
+                "circulating_supply": 1.23,
+                "fully_diluted_valuation": 1.23,
+                "id": "test_id",
+                "image": "test_img_url",
+                "market_cap": 10,
+                "market_cap_rank": 1,
+                "max_supply": 1.23,
+                "mc_fdv_ratio": 8.13,
+                "symbol": "test_symbol",
+                "total_supply": 1.23,
+                "total_volume": 1.23,
             },
-            "tokenomics": {
-                "test_id": {
-                    "circ_supply_total_supply_ratio": 1.0,
-                    "circulating_supply": 1.23,
-                    "fully_diluted_valuation": 1.23,
-                    "id": "test_id",
-                    "image": "test_img_url",
-                    "market_cap": 10,
-                    "market_cap_rank": 1,
-                    "max_supply": 1.23,
-                    "mc_fdv_ratio": 8.13,
-                    "symbol": "test_symbol",
-                    "total_supply": 1.23,
-                    "total_volume": 1.23,
-                },
-            },
-            "order": ["test_id"],
-            "updated_at": UPDATED_AT,
-        }
+        },
+        "order": ["test_id"],
+        "updated_at": UPDATED_AT,
+    }
 
-        assert response.json["updated_at"] == str(UPDATED_AT)
+    assert response.json["updated_at"] == str(UPDATED_AT)
 
 
+# FIXME: This test is not working yet. We need to find a way to run the event stream in
+# the background.
 @pytest.mark.skip
 def test_get_coins_stream(client, app):
+    "Test the coin event stream."
+
+    async def send_pubsub():
+        pub_payload = {
+            "data": {
+                "changed": 1,
+                "updated_at": UPDATED_AT,
+            },
+            "errors": [],
+        }
+
+        while True:
+            await sleep(0.1)
+            print("publishing")
+
+            app.redis_conn.publish("coins", json.dumps(pub_payload))
+
+    queue = Queue(is_async=False, connection=app.redis_conn)
+    job = queue.enqueue(
+        send_pubsub,
+    )
+    assert job
+
     with mock_events():
         response = client.get(
             "/api/coin-stream", headers={"Accept": "text/event-stream"}
         )
 
-        app.redis_conn.publish("coins")
-        # app.redis_conn.set("coins:all", json.dumps([]))
-
-        assert response.status_code == 200
-        assert response.json == {}
-
-        # assert response.content_type == "text/event-stream"
-        # assert response.headers["Cache-Control"] == "no-cache"
-        # assert response.headers["Connection"] == "keep-alive"
-        # assert response.headers["X-Accel-Buffering"] == "no"
+    assert response.status_code == 200
+    # assert response.content_type == "text/event-stream"
+    # assert response.headers["Cache-Control"] == "no-cache"
+    # assert response.headers["Connection"] == "keep-alive"
+    # assert response.headers["X-Accel-Buffering"] == "no"
--- database/tests/test_helpers.py
+++ database/tests/test_helpers.py
@@ -8,6 +8,7 @@ from utils.helpers import (
 
 @pytest.fixture
 def coin_data():
+    """Fixture for the coin data with some fictional properties."""
     return [
         {
             "prop1": 10.0,  # Different signs should generate two independent list
@@ -28,6 +29,14 @@ def coin_data():
 
 
 def test_process_percentages(coin_data):
+    """Test the process_percentages function.
+
+    Args:
+        coin_data (list): List of coin data.
+
+    Returns:
+        None
+    """
     expected_output = [
         {
             "prop1": 10.0,
@@ -74,6 +83,33 @@ def test_process_percentages(coin_data):
 
 
 def test_compute_extra_columns():
+    """Test case for the compute_extra_columns function.
+
+    This test case verifies that the compute_extra_columns function correctly
+    computes the extra columns for the given input data and returns the expected
+    output.
+
+    The input data consists of a list of dictionaries, where each dictionary
+    represents a data entry with various properties such as market_cap,
+    fully_diluted_valuation, circulating_supply, and total_supply.
+
+    The expected output is a list of dictionaries, where each dictionary
+    represents a data entry with the computed extra columns such as mc_fdv_ratio
+    and circ_supply_total_supply_ratio which are computed from the input data.
+    """
+    input_data = [
+        {
+            "market_cap": 428569062162,
+            "fully_diluted_valuation": 466538642853,
+            "circulating_supply": 251816.736,
+            "total_supply": 251816.736,
+        },
+        {
+            "market_cap": 188109860328,
+            "fully_diluted_valuation": 188109860328,
+        },
+    ]
+
     expected = [
         {
             "market_cap": 428569062162,
@@ -91,22 +127,16 @@ def test_compute_extra_columns():
         },
     ]
 
-    input_data = [
-        {
-            "market_cap": 428569062162,
-            "fully_diluted_valuation": 466538642853,
-            "circulating_supply": 251816.736,
-            "total_supply": 251816.736,
-        },
-        {
-            "market_cap": 188109860328,
-            "fully_diluted_valuation": 188109860328,
-        },
-    ]
-    assert expected == compute_extra_columns(input_data)
+    assert compute_extra_columns(input_data) == expected
 
 
 def test_generate_diff_all_equal():
+    """Return an empty dictionary when the two data objects are equal.
+
+    The expected behavior is that the function should return an empty
+    dictionary, indicating that there are no differences between the two data
+    objects.
+    """
     previous_data = current_data = {
         "prices": {
             "bitcoin": {
@@ -312,6 +342,314 @@ def test_generate_diff_one_different():
                 "max_supply": 21000000,
                 "total_volume": 14118300117,
             }
+        },
+    }
+
+    assert generate_object_diff(previous_data, current_data) == expected_diff
+
+
+def test_generate_diff_one_new_coins():
+    """Test case for generating the difference between previous and current data
+    when there is one new coin added.
+
+    In this case we have a change in the first coin's
+    price_change_percentage_1h_in_currency and a new coin added to the data.
+    Both should be returned in the diff.
+    """
+    previous_data = {
+        "prices": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap_rank": 1,
+                "current_price": 27301,
+                "price_change_percentage_1h_in_currency": -0.1564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+        },
+        "tokenomics": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 1,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+        },
+    }
+
+    current_data = {
+        "prices": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap_rank": 1,
+                "current_price": 27301,
+                "price_change_percentage_1h_in_currency": -0.2564735720678735,  # This is the only difference
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+            "etherium": {
+                "id": "etherium",
+                "symbol": "eth",
+                "name": "Etherium",
+                "image": "https://assets.coingecko.com/coins/images/1/large/etherium.png?1547033579",
+                "market_cap_rank": 2,
+                "current_price": 7301,
+                "price_change_percentage_1h_in_currency": -0.1564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+        },
+        "tokenomics": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 1,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+            "etherium": {
+                "id": "etherium",
+                "symbol": "eth",
+                "name": "Etherium",
+                "image": "https://assets.coingecko.com/coins/images/1/large/etherium.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 2,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+        },
+    }
+
+    expected_diff = {
+        "prices": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap_rank": 1,
+                "current_price": 27301,
+                "price_change_percentage_1h_in_currency": -0.2564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+            "etherium": {
+                "id": "etherium",
+                "symbol": "eth",
+                "name": "Etherium",
+                "image": "https://assets.coingecko.com/coins/images/1/large/etherium.png?1547033579",
+                "market_cap_rank": 2,
+                "current_price": 7301,
+                "price_change_percentage_1h_in_currency": -0.1564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+        },
+        "tokenomics": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 1,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+            "etherium": {
+                "id": "etherium",
+                "symbol": "eth",
+                "name": "Etherium",
+                "image": "https://assets.coingecko.com/coins/images/1/large/etherium.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 2,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+        },
+    }
+
+    assert generate_object_diff(previous_data, current_data) == expected_diff
+
+
+def test_generate_diff_one_coin_removed():
+    """Test case for generating the difference between previous and current data
+    when there is a coin removed.
+
+    In this case we have a change in the first coin's
+    price_change_percentage_1h_in_currency and one coins from the previous run
+    was removed.  The diff should only return the first coin, in the second one
+    we're not interested anymore.
+    """
+    previous_data = {
+        "prices": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap_rank": 1,
+                "current_price": 27301,
+                "price_change_percentage_1h_in_currency": -0.1564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+            "etherium": {
+                "id": "etherium",
+                "symbol": "eth",
+                "name": "Etherium",
+                "image": "https://assets.coingecko.com/coins/images/1/large/etherium.png?1547033579",
+                "market_cap_rank": 2,
+                "current_price": 7301,
+                "price_change_percentage_1h_in_currency": -0.1564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+        },
+        "tokenomics": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 1,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+            "etherium": {
+                "id": "etherium",
+                "symbol": "eth",
+                "name": "Etherium",
+                "image": "https://assets.coingecko.com/coins/images/1/large/etherium.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 2,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+        },
+    }
+
+    current_data = {
+        "prices": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap_rank": 1,
+                "current_price": 27301,
+                "price_change_percentage_1h_in_currency": -0.2564735720678735,  # This is the only difference
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+        },
+        "tokenomics": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 1,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
+        },
+    }
+
+    expected_diff = {
+        "prices": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap_rank": 1,
+                "current_price": 27301,
+                "price_change_percentage_1h_in_currency": -0.2564735720678735,
+                "price_change_percentage_24h_in_currency": -2.248931288518162,
+                "price_change_percentage_7d_in_currency": 4.360420461311904,
+                "price_change_percentage_30d_in_currency": 5.360420461311904,
+                "price_change_percentage_1y_in_currency": 6.360420461311904,
+                "ath_change_percentage": 0.23,
+            },
+        },
+        "tokenomics": {
+            "bitcoin": {
+                "id": "bitcoin",
+                "symbol": "btc",
+                "name": "Bitcoin",
+                "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                "market_cap": 533052722379,
+                "market_cap_rank": 1,
+                "fully_diluted_valuation": 573962971793,
+                "circulating_supply": 19503187,
+                "total_supply": 21000000,
+                "max_supply": 21000000,
+                "total_volume": 14118300117,
+            },
         },
     }
 
